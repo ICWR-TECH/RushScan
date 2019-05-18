316477105a4710a188166399d10fe1dc61c80c1e
Afrizal F.A <42967700+Afrizall@users.noreply.github.com>
2019-05-18 17:59:37 +0700

    Update rush.py

--- rush.py
+++ rush.py
@@ -20,6 +20,8 @@ user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Geck
 str_cms=open("lib/str_cms.rush", "r").read()
 linker_str="""href=\"(.+?)\"^^href=\'(.+?)\'""".split("^^")
 konten_str="""src=\"(.+?)\"^^src=\'(.+?)\'""".split("^^")
+p_str="""name=\"(.+?)\"^^name=\'(.+?)\'""".split("^^")
+pyld_xss="<!-- XSS -->"
 print "[*] Scanning : " + target
 konten=requests.get(url=target, headers={"User-Agent" : user_agent}, allow_redirects=True).content
 cek=""
@@ -32,9 +34,9 @@ for key in pisah_cms :
     konten_cms=requests.get(url=target + "/robots.txt", headers={"User-Agent" : user_agent}, allow_redirects=True).content
     if re.search(cms_value[0], konten_cms) :
         cms_detect=cms_value[1]
-        print "[+] CMS : " + cms_value[1]
+        print "[+] CMS : " + cms_value[2]
         print "\n[*] Scanning Sensitive " + cms_detect + " Plugins"
-        list_plugins=open(cms_value[2], "r").read().split("^^^")
+        list_plugins=open(cms_value[1], "r").read().split("^^^")
         p_detect=""
         for p_key in list_plugins :
             s_plugins=p_key.split("^")
@@ -44,7 +46,7 @@ for key in pisah_cms :
                 print "\n[+] Find Plugins : " + s_plugins[1]
                 print "[+] Reference : " + s_plugins[2]
         if not p_detect :
-            print "\n[+] Not Detect Plugins"
+            print "\n[-] Not Detect Plugins"
 
 if not cms_detect :
     print "[-] CMS Not Detected"
@@ -57,14 +59,14 @@ for x_linker in linker_str :
         result_f=link_f
         if re.match(target, link_f) :
             if re.match("http://", link_f) or re.match("https://", link_f) :
-                print "[+] Content : " + link_f.replace("///", "/")
+                print "[+] Find Content : " + link_f.replace("///", "/")
         else :
             if re.match("http://", link_f) or re.match("https://", link_f) :
-                print "[+] Content : " + link_f.replace("///", "/")
+                print "[+] Find Content : " + link_f.replace("///", "/")
             else :
                 e_link_f_raw=target + "/" + link_f
                 e_link_f=e_link_f_raw.replace("///", "/")
-                print "[+] Content : " + e_link_f
+                print "[+] Find Content : " + e_link_f
 
     if not result_f :
         print "[-] Content Not Detected"
@@ -88,3 +90,25 @@ for x_konten in linker_str :
 
     if not result_l :
         print "[-] URL Not Detected"
+
+x_s=""
+print "\n[*] Detect Parameter\n"
+for x_str in p_str :
+    m_param=re.findall(x_str, konten)
+    for m_p in m_param :
+        x_s = { m_p : pyld_xss }
+        print "[+] Find : " + m_p
+        req_post=requests.post(url=target, data=x_s, headers={"User-Agent" : user_agent}, allow_redirects=True).content
+        if re.search(pyld_xss, req_post) :
+            print "[+] Vulnerability In Parameter \"" + x_s[m_p] + "\""
+        else :
+            print "[-] Failed Trying XSS ( POST ) Parameter : " + m_p
+        xss_get=target + "?" + str(m_p) + "=" + x_s[m_p]
+        req_get=requests.post(url=xss_get, headers={"User-Agent" : user_agent}, allow_redirects=True).content
+        if re.search(pyld_xss, req_get) :
+            print "[+] Vulnerability Parameter \"" + m_p + "\" In URL : " + xss_get
+        else :
+            print "[*] Failed Trying XSS ( GET ) Parameter " + str(m_p) + " : " + xss_get
+
+if not x_s :
+    print "[-] Parameter Not Found"
