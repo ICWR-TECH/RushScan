5ec8ae0023eac1428c26e37b2fb39fd858ea302d
Afrizal F.A <42967700+Afrizall@users.noreply.github.com>
2019-05-24 03:52:52 +0700

    Update rush.py

--- rush.py
+++ rush.py
@@ -20,6 +20,7 @@ user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Geck
 str_cms=open("lib/str_cms.rush", "r").read()
 linker_str="""href=\"(.+?)\"^^href=\'(.+?)\'""".split("^^")
 konten_str="""src=\"(.+?)\"^^src=\'(.+?)\'""".split("^^")
+action_str="""action=\"(.+?)\"^^action=\'(.+?)\'""".split("^^")
 p_str="""name=\"(.+?)\"^^name=\'(.+?)\'""".split("^^")
 pyld_xss="<!-- XSS -->"
 print "[*] Scanning : " + target
@@ -51,9 +52,9 @@ for key in pisah_cms :
 if not cms_detect :
     print "[-] CMS Not Detected"
 
+result_f=""
 print "\n[*] Scanning Content\n"
 for x_linker in linker_str :
-    result_f=""
     str_link=re.findall(x_linker, konten)
     for link_f in str_link :
         result_f=link_f
@@ -68,12 +69,12 @@ for x_linker in linker_str :
                 e_link_f=e_link_f_raw.replace("///", "/")
                 print "[+] Find Content : " + e_link_f
 
-    if not result_f :
-        print "[-] Content Not Detected"
+if not result_f :
+    print "[-] Content Not Detected"
 
+result_l=""
 print "\n[*] Scanning URL\n"
 for x_konten in linker_str :
-    result_l=""
     str_link=re.findall(x_konten, konten)
     for link in str_link :
         result_l=link
@@ -88,28 +89,99 @@ for x_konten in linker_str :
                 e_link=e_link_raw.replace("///", "/")
                 print "[+] Find URL : " + e_link
 
-    if not result_l :
-        print "[-] URL Not Detected"
+if not result_l :
+    print "[-] URL Not Detected"
+
+result_a=""
+link_a=""
+print "\n[*] Scanning Action URL\n"
+for x_action in action_str :
+    str_link=re.findall(x_action, konten)
+    for link in str_link :
+        result_a=link
+        if re.match(target, result_a) :
+            if re.match("http://", link) or re.match("https://", link) :
+                link_a+=link.replace("///", "/") + "\n"
+                print "[+] Find Action URL : " + link.replace("///", "/")
+        else :
+            if re.match("http://", link) or re.match("https://", link) :
+                link_a+=link.replace("///", "/") + "\n"
+                print "[+] Find Action URL : " + link.replace("///", "/")
+            else :
+                a_link_raw=target + "/" + link
+                a_link=e_link_raw.replace("///", "/")
+                link_a+=a_link + "\n"
+                print "[+] Find Action URL : " + a_link
+
+if not result_a :
+    print "[-] Action URL Not Detected"
 
 x_s=""
+xg_x=""
 print "\n[*] Detect Parameter\n"
 for x_str in p_str :
     m_param=re.findall(x_str, konten)
+    xp={ "" : ""}
+    xg=""
     for m_p in m_param :
+        dict={ m_p : pyld_xss }
         x_s = { m_p : pyld_xss }
+        xg_x=m_p + "=" + pyld_xss
+        xg+="&" + m_p + "=" + pyld_xss
+        xp.update(dict)
         print "[+] Find : " + m_p
         req_post=requests.post(url=target, data=x_s, headers={"User-Agent" : user_agent}, allow_redirects=True).content
         if re.search(pyld_xss, req_post) :
             print "[+] Vulnerability In Parameter \"" + x_s[m_p] + "\""
+            ask=raw_input("Type \"exit\" For Exit : ")
+            if ask == "exit" :
+                exit
         else :
             print "[-] Failed Trying XSS ( POST ) Parameter : " + m_p
         xss_get=target + "/?" + str(m_p) + "=" + x_s[m_p]
         req_get=requests.post(url=xss_get, headers={"User-Agent" : user_agent}, allow_redirects=True).content
         if re.search(pyld_xss, req_get) :
             print "[+] Vulnerability Parameter \"" + m_p + "\" In URL : " + xss_get
+            ask=raw_input("Type \"exit\" For Exit : ")
+            if ask == "exit" :
+                exit
         else :
             print "[*] Failed Trying XSS ( GET ) Parameter " + str(m_p) + " : " + xss_get
 
+print "\n[*] Trying All Parameter Post Data\n"
+if re.search(pyld_xss, requests.post(url=target, data=xp, headers={"User-Agent" : user_agent}, allow_redirects=True).content) :
+    print "[+] Find POST Request Xss In : " + target
+else :
+    print "[-] Not Find POST Request Xss"
+
+print "\n[*] Trying All Parameter GET\n"
+if re.search(pyld_xss, requests.get(url=target + "?" + xg_x + xg, headers={"User-Agent" : user_agent}, allow_redirects=True).content) :
+    print "[+] Find GET Request Xss In : " + target
+else :
+    print "[-] Not Find GET Request Xss"
+
 if not x_s :
-    print "[-] Parameter Not Found"
-  
+    print "\n[-] Parameter Not Found\n"
+
+link_act=link_a.split("\n")
+if link_a :
+    for l_act in link_act :
+        if not l_act :
+            continue
+        print "\n[*] Trying All Parameter POST in URL : " + l_act + "\n"
+        if re.search(pyld_xss, requests.post(url=l_act, data=xp, headers={"User-Agent" : user_agent}, allow_redirects=True).content) :
+            print "[+] Find POST Request Xss In : " + l_act
+            ask=raw_input("Type \"exit\" For Exit : ")
+            if ask == "exit" :
+                exit
+        else :
+            print "[-] Not Find POST Request Xss"
+
+        print "\n[*] Trying All Parameter GET in URL : " + l_act + "\n"
+        if re.search(pyld_xss, requests.get(url=l_act + "?" + xg_x + xg, headers={"User-Agent" : user_agent}, allow_redirects=True).content) :
+            print "[+] Find GET Request Xss In : " + target
+            ask=raw_input("Type \"exit\" For Exit : ")
+            if ask == "exit" :
+                exit
+        else :
+            print "[-] Not Find GET Request Xss"
